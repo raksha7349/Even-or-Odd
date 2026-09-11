1625c8ed3a2ec46b93a09696f2d5d83d31cdbc29
raksha7349 <rakshapatil7349@gmail.com>
2026-09-11 15:39:45 +0530

    Update EvenorOdd.py

--- EvenorOdd.py
+++ EvenorOdd.py
@@ -2,6 +2,7 @@ num = int(input("Enter a number: "))
 
 if num % 2 == 0:
     print("Even")
+    print("It is Even number".)
 else:
-  print("Odd")
-  
+    print("Odd")
+    print("It is Even number".)
